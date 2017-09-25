2f7db5aca5fce490f143f4595360d89bbae38bc3
Piotr Sekula <piotr92345@o2.pl>
2017-09-25 18:39:29 +0200

    Delete unnecessary form fields

--- src/main/java/com/packt/webstore/controller/ProductController.java
+++ src/main/java/com/packt/webstore/controller/ProductController.java
@@ -9,6 +9,9 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import com.packt.webstore.domain.Product;
@@ -18,72 +21,82 @@ import com.packt.webstore.service.ProductService;
 @RequestMapping("/products")
 public class ProductController {
 
-	@Autowired
-	private ProductService productService;
+    @Autowired
+    private ProductService productService;
 
-	@RequestMapping()
-	public String list(Model model) {
-		model.addAttribute("products", productService.getAllProducts());
+    @RequestMapping()
+    public String list(Model model) {
+        model.addAttribute("products", productService.getAllProducts());
 
-		return "products";
-	}
-	
-	@RequestMapping("/all")
-	public String allProducts(Model model) {
-		model.addAttribute("products", productService.getAllProducts());
+        return "products";
+    }
 
-		return "products";
-	}
-	
-	@RequestMapping("/{category}")
-	public String getProductsByCategory(Model model, 
-			@PathVariable("category") String productCategory) {
-		model.addAttribute("products", productService.getProductsByCategory(productCategory));
-		return "products";
-	}
-	
-	@RequestMapping("/filter/{ByCriteria}")
-	public String getProductsByFilter(@MatrixVariable(pathVar="ByCriteria")
-	Map<String, List<String>> filterParams, Model model) {
-		model.addAttribute("products", productService.getProductByFilter(filterParams));
-		return "products";
-	}
-	
-	@RequestMapping("/{category}/{price}")
-	public String filterProducts(Model model,
-			@PathVariable("category") String productCategory,
-			@MatrixVariable(pathVar="price") Map<String, List<String>> pricesRange,
-			@RequestParam("manufacturer") String manufacturer) {
+    @RequestMapping("/all")
+    public String allProducts(Model model) {
+        model.addAttribute("products", productService.getAllProducts());
 
-		Set<Product> productsInPriceRange = new HashSet<>();
-		productsInPriceRange.addAll(productService.getProductByPriceFilter(pricesRange));
+        return "products";
+    }
 
-		Set<Product> filteredProducts = productsInPriceRange.stream()
-				.filter(product -> productService.getProductsByCategory(productCategory).contains(product))
-				.filter(product -> productService.getProductsByManufacturer(manufacturer).contains(product))
-				.collect(Collectors.toSet());
+    @RequestMapping("/{category}")
+    public String getProductsByCategory(Model model,
+                                        @PathVariable("category") String productCategory) {
+        model.addAttribute("products", productService.getProductsByCategory(productCategory));
+        return "products";
+    }
 
-		model.addAttribute("products", filteredProducts);
-		
-		return "products";
-	}
-	
-	@RequestMapping("/product")
-	public String getProductById(@RequestParam("id") String productId, Model model) {
-		model.addAttribute("product", productService.getProductById(productId));
-		return "product";
-	}
+    @RequestMapping("/filter/{ByCriteria}")
+    public String getProductsByFilter(@MatrixVariable(pathVar = "ByCriteria")
+                                              Map<String, List<String>> filterParams, Model model) {
+        model.addAttribute("products", productService.getProductByFilter(filterParams));
+        return "products";
+    }
 
-	@RequestMapping(value = "/add", method = RequestMethod.GET)
-    public String getAddNewProduct(Model model) {
-	    Product newProduct = new Product();
-	    model.addAttribute("newProduct", newProduct);
-	    return "addProduct";
+    @RequestMapping("/{category}/{price}")
+    public String filterProducts(Model model,
+                                 @PathVariable("category") String productCategory,
+                                 @MatrixVariable(pathVar = "price") Map<String, List<String>> pricesRange,
+                                 @RequestParam("manufacturer") String manufacturer) {
+
+        Set<Product> productsInPriceRange = new HashSet<>();
+        productsInPriceRange.addAll(productService.getProductByPriceFilter(pricesRange));
+
+        Set<Product> filteredProducts = productsInPriceRange.stream()
+                .filter(product -> productService.getProductsByCategory(productCategory).contains(product))
+                .filter(product -> productService.getProductsByManufacturer(manufacturer).contains(product))
+                .collect(Collectors.toSet());
+
+        model.addAttribute("products", filteredProducts);
+
+        return "products";
+    }
+
+    @RequestMapping("/product")
+    public String getProductById(@RequestParam("id") String productId, Model model) {
+        model.addAttribute("product", productService.getProductById(productId));
+        return "product";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String getAddNewProductForm(Model model) {
+        Product newProduct = new Product();
+        model.addAttribute("newProduct", newProduct);
+        return "addProduct";
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String processAddNewProductForm(@ModelAttribute("newProduct") Product newProduct) {
-	    productService.addProduct(newProduct);
-	    return "redirect:/products";
+    public String processAddNewProductForm(@ModelAttribute("newProduct") Product newProduct,
+                                           BindingResult result) {
+        String[] suppressedFields = result.getSuppressedFields();
+        if (suppressedFields.length > 0) {
+            throw new RuntimeException("Attempt of binding suppressed fields: " + StringUtils.arrayToCommaDelimitedString(suppressedFields));
+        }
+        productService.addProduct(newProduct);
+        return "redirect:/products";
+    }
+
+    @InitBinder
+    public void initializeBinder(WebDataBinder binder) {
+        binder.setDisallowedFields("unitsInOrder", "discontinued");
     }
 }
